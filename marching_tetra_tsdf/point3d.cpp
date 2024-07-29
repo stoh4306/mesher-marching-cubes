ba4484a279fba9ee9fa1e232d15b0a7b4196d5d9
Seungtaik Oh <stoh@me.com>
2024-07-30 08:34:17 +0900

    Update copyright remark

--- marching_tetra_tsdf/point3d.cpp
+++ marching_tetra_tsdf/point3d.cpp
@@ -7,8 +7,6 @@ For further information, contact:
 Dr. Seungtaik Oh
 
 E-mail: seung.oh@icloud.com, ogong36@gmail.com
-Phone : +82 10 2923 6290
-Address: 217-611 31 Shinbanpo-ro23gil Seocho Seoul 06509 KOREA
 
 This copyright notice must be included with all copies of the source code.
 ===========================================================================*/
